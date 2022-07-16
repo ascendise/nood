97f2e218a6d85bb645a72323f550eab6980ec92e
Ascendise <rapperido@gmail.com>
2022-07-17 01:54:12 +0200

    Have working login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthGuard } from './auth.guard';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
@@ -15,9 +15,11 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    OAuthModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +8,25 @@ import { AuthService } from '../services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private readonly authConfig: AuthConfig = {
+    issuer: 'https://dev-ypizy20t.us.auth0.com',
+    redirectUri: 'http://localhost:4200',
+    clientId: 'TzjUFvs2J6uDTfbDsJOJa3iKoXvW49wj',
+    scope: 'openid profile email offline_access',
+    responseType: 'code',
+    loginUrl: 'https://dev-ypizy20t.us.auth0.com/authorize'
+  }
+
+  constructor(private oauthService: OAuthService)
+  {
+    this.oauthService.configure(this.authConfig)
+  }
 
   ngOnInit(): void {
+  }
+
+  onSignIn() {
+    this.oauthService.initCodeFlow()
   }
 
 }
