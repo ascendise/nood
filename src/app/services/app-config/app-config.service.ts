31bfb904988926596d341f7c42947886466e4de9
Ascendise <rapperido@gmail.com>
2022-08-14 04:43:40 +0200

    service for loading app config

--- src/app/services/app-config/app-config.service.ts
+++ src/app/services/app-config/app-config.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppConfigService {
 
-  constructor() { }
+  private static readonly configFile = '/assets/config.json';
+
+  constructor(private client: HttpClient) { }
+
+  async loadConfig(): Promise<AppConfig> {
+    return firstValueFrom(this.client.get<AppConfig>(AppConfigService.configFile));
+  }
+}
+
+export interface AppConfig {
+  apiBaseUri: string,
+  oauth: OAuthConfig,
+}
+
+export interface OAuthConfig {
+  issuer: string,
+  clientId: string,
+  logoutUrl: string,
+  loginUrl: string,
+  audience: string,
 }
