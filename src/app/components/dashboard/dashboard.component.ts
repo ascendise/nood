f355a7674ea0aad90d76e8d04f22030f774db551
ascendise <rapperido@gmail.com>
2022-08-17 16:51:56 +0200

    wip: tasks on dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskEntity, TasksService } from 'src/app/services/tasks/tasks.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+
+  private _tasks: TaskEntity[] = [];
+
+  constructor(private tasksService: TasksService) { }
+
+  async ngOnInit() {
+    this._tasks = await this.tasksService.getTasks();
+  }
+
+  public get tasks(): TaskEntity[] {
+    return this._tasks;
+  }
+
+}
