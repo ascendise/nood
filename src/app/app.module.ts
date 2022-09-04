d79f4cd6372aea34c030b4d469e5a4ab0446091b
Ascendise <rapperido@gmail.com>
2022-09-04 22:28:49 +0200

    Moved validator into own folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AnonymousComponent } from './components/anonymous/anonymous.component';
 import { NewTaskComponent } from './components/task/new-task/new-task.component';
 import { FormsModule } from '@angular/forms';
-import { NotInPastDirective } from './validators/not-in-past.directive';
+import { NotInPastDirective } from './validators/not-in-past/not-in-past.directive';
 import { TaskDetailsComponent } from './components/task/task-details/task-details.component';
 import { EditTaskComponent } from './components/task/edit-task/edit-task.component';
 
