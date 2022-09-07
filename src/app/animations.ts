0a36d5d3bb55704ccf7cc7709b696ee01b93e73d
Ascendise <rapperido@gmail.com>
2022-09-07 13:06:50 +0000

    Move to right

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -8,20 +8,20 @@ export const routeAnimations =
         style({
           position: 'absolute',
           top: 0,
-          left: 0,
+          right: 0,
           width: '100%'
         })
       ]),
       query(':enter', [
-        style({ left: '-100%' })
+        style({ right: '-100%' })
       ]),
       query(':leave', animateChild()),
       group([
         query(':leave', [
-          animate('200ms ease-out', style({ left: '100%', opacity: 0 }))
+          animate('200ms ease-out', style({ right: '100%', opacity: 0 }))
         ]),
         query(':enter', [
-          animate('300ms ease-out', style({ left: '0%' }))
+          animate('300ms ease-out', style({ right: '0%' }))
         ]),
         query('@*', animateChild(), { optional: true })
       ]),
