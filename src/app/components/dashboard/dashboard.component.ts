226ebcbf29dda918f8af0995d314dffe1bddc12a
Ascendise <rapperido@gmail.com>
2022-09-17 19:43:48 +0200

    List all available checklists

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ChecklistEntity, ChecklistsService } from 'src/app/services/checklists/checklists.service';
 import { TaskEntity, TasksService } from 'src/app/services/tasks/tasks.service';
 
 @Component({
@@ -9,15 +10,21 @@ import { TaskEntity, TasksService } from 'src/app/services/tasks/tasks.service';
 })
 export class DashboardComponent implements OnInit {
   private _tasks: TaskEntity[] = [];
+  private _checklists: ChecklistEntity[] = [];
 
-  constructor(private tasksService: TasksService, private router: Router) {}
+  constructor(private tasksService: TasksService, private checklistsService: ChecklistsService, private router: Router) {}
 
   async ngOnInit() {
     this._tasks = await this.tasksService.getTasks();
+    this._checklists = await this.checklistsService.getChecklists();
   }
 
   public get tasks(): TaskEntity[] {
     return this._tasks;
+  }
+
+  public get checklists(): ChecklistEntity[] {
+    return this._checklists;
   }
 
   public navigateToTaskDetails(task: TaskEntity) {
