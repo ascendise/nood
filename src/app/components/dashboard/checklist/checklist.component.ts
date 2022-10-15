dc269b19a3dc723fb0b149498d3113c4aab7c23e
Ascendise <rapperido@gmail.com>
2022-10-15 15:17:23 +0200

    Checking box changes task to isDone=true

--- src/app/components/dashboard/checklist/checklist.component.ts
+++ src/app/components/dashboard/checklist/checklist.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { ChecklistEntity } from 'src/app/services/checklists/checklists.service';
-import { TaskEntity } from 'src/app/services/tasks/tasks.service';
+import { TaskEntity, TasksService, Task } from 'src/app/services/tasks/tasks.service';
 
 @Component({
   selector: 'app-checklist',
@@ -13,7 +13,7 @@ export class ChecklistComponent {
   @Input() public title = '';
   @Input() public checklist?: ChecklistEntity;
 
-  constructor(private router: Router) {}
+  constructor(private taskService: TasksService, private router: Router) {}
 
   public navigateToTaskDetails(task: TaskEntity) {
     this.router.navigateByUrl('/task-details', { state: task });
@@ -21,5 +21,18 @@ export class ChecklistComponent {
 
   public navigateToChecklist() {
     this.router.navigateByUrl('/edit-checklist', { state: this.checklist });
+  }
+
+  public async toggleDone(task: TaskEntity) {
+    //TODO: Add method to TaskEntity to get task from it
+    task.isDone = true;
+    const updatedTask: Task = {
+      name: task.name,
+      description: task.description,
+      startDate: new Date(task.startDate),
+      endDate: task.endDate ? new Date(task.endDate) : null,
+      isDone: task.isDone
+    }
+    await this.taskService.updateTask(updatedTask, task._links);
   }
 }
